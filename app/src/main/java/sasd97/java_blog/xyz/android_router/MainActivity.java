f681f876583bd3251d21e6a3c405bb30edc4f985
alexander <st235@yandex.ru>
2017-09-05 23:27:15 +0300

    An empty project had been created

---
+++ app/src/main/java/sasd97/java_blog/xyz/android_router/MainActivity.java
@@ -0,0 +1,13 @@
+package sasd97.java_blog.xyz.android_router;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
